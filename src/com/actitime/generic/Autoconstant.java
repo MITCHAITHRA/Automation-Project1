1274336cf285fcbb9377cbc5a8655caf30448143
tyss <tyss@localhost>
2018-04-25 14:24:35 +0530

    1st commit

---
+++ src/com/actitime/generic/Autoconstant.java
@@ -0,0 +1,9 @@
+package com.actitime.generic;
+
+public interface Autoconstant 
+{
+String chrome_key="webdriver.chrome.driver";
+String chrome_value="./drivers/chromedriver.exe";
+String gecko_key="webdriver.gecko.driver";
+String gecko_value="./drivers/geckodriver.exe";
+}
